a7754f0b3ceee749e985f21f53597507258835e3
MauricioEnLaUni <210804@utags.edu.mx>
2023-10-29 12:47:56 -0600

    Updated services AES

--- authService/src/lib/infrastructure/encryption/AES.ts
+++ authService/src/lib/infrastructure/encryption/AES.ts
@@ -4,26 +4,54 @@ import { decode, encode } from "@msgpack/msgpack";
 
 export default class AES
 {
-    private method = process.env.CRYPTO_METHOD as string;
-    private key = process.env.AES_KEY as string;
-    private iv = process.env.AES_IV as string;
+    private static method = process.env.CRYPTO_METHOD as string;
+    private static key = process.env.AES_KEY as string;
 
-    encrypt(data: any)
+    private static sign(data: Buffer)
     {
-        const cipher = crypto.createCipheriv(this.method, this.key, this.iv);
-        const msg = encode(data);
-        
-        return Buffer
-            .concat([cipher.update(msg), cipher.final()])
-            .toString("base64");
+        const hmac = crypto
+            .createHmac("sha256", AES.key)
+            .update(data)
+            .digest();
+        return Buffer.concat([data, hmac]);
     }
 
-    decrypt(data: string)
+    private static verify(data: Buffer, digest: Buffer)
     {
-        const buffer = Buffer.from(data, "base64");
-        const decipher = crypto.createDecipheriv(this.method, this.key, this.iv);
+        const hmac = crypto.createHmac("sha256", AES.key);
+        
+        return hmac.update(data).digest() === digest;
+    }
 
-        const decrypted = Buffer.concat([decipher.update(buffer), decipher.final()]);
+    static encrypt(data: any)
+    {
+        const iv = crypto.randomBytes(16);
+        const cipher = crypto.createCipheriv(AES.method, AES.key, iv);
+        const msg = encode(data);
+        
+        const encrypted = Buffer
+            .concat([cipher.update(msg), cipher.final(), iv]);
+
+        return AES.sign(encrypted);
+    }
+
+    static decrypt(data: Buffer, iv: Buffer)
+    {
+        const decipher = crypto.createDecipheriv(AES.method, AES.key, iv);
+
+        return Buffer.concat([decipher.update(data), decipher.final()]);
+    }
+
+    static isSecure(data: Buffer)
+    {
+        const digest = data.subarray(-32);
+        if (!AES.verify(data, digest))
+        {
+            return undefined;
+        }
+
+        const iv = data.subarray(-48, -32);
+        const decrypted = AES.decrypt(data, iv);
 
         return decode(decrypted);
     }
